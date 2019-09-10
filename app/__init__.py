ea27f816564e25c1a14c1d2c5c719f5a1e13ceac
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-09-10 19:55:27 +0300

    setup sending of sms

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 from app.config import app_config
 from app.users import users
+from flask_apscheduler import APScheduler
+
+
+def registerschedule(app):
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
 
 
 def create_app(config_name):
@@ -9,6 +16,7 @@ def create_app(config_name):
     conf = app.config.from_object(app_config)
     app.config.from_pyfile('config.py')
     register_blueprints(app)
+    registerschedule(app)
     return app
 
 
--- app/crash_report/views.py
+++ app/crash_report/views.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse
-from flask import make_response, request
+from flask import make_response, request, current_app
 from sqlalchemy import exc
 from datetime import datetime
+from app.sms_utils import MessageClient
 
 from app.database import db
 
@@ -14,8 +15,25 @@ class CrushReport(Resource):
         now = datetime.now()
         args['created_at'] = now.strftime("%m-%d-%Y %H:%M:%S")
         res = db.child("reports").push(args)
+        tag = args.get('device_tag')
+        current_app.apscheduler.add_job(func=sendsms,
+                                        trigger='date',
+                                        args=[tag, args.get('location')], id='j'+str(res.get('name')))
+
         return make_response({"message": "Incident reported"}, 201)
 
     def get(self):
         response = db.child('reports').get()
         return response.val()
+
+
+def sendsms(tag, location):
+    url = f"https://www.google.com/maps?q={location.get('latitude')},{location.get('longitude')}"
+    users = db.child('users').get().val()
+    messenger = MessageClient()
+    user_to_in_danger = [user
+                         for user in users.values()
+                         if user.get('device_tag') == tag]
+    body = f"ALERT !!!!\n{user_to_in_danger[0].get('username')} might be danger\nContact them on phone number {user_to_in_danger[0].get('phone_number')}\nUser is at location {url}"  # noqa
+    for number in user_to_in_danger[0].get('alert_receivers'):
+        messenger.send_message(body, to=number)
